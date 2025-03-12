1b991b5f5eb6629bd93f7f2bf5866929e9c878b6
Michele <michele5rosato@gmail.com>
2025-03-12 12:53:47 +0100

    NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,42 @@
-import "./Navbar.css";
-import logo from "./img/logo.png";
-import React from "react";
+import { useState, useEffect } from "react";
+import { FaSearch, FaUser, Circle, FaThLarge } from "react-icons/fa";
+import "./NavBar.css";
 
-function NavBar() {
+const NavBar = () => {
+  const [scrolling, setScrolling] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolling(true);
+      } else {
+        setScrolling(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div className="Navbar">
-        <a href="#Home">
-            <img src="{logo}" alt="logo" />
-        </a>
-        <ul>
-            <li><a href="#">Film</a></li>
-            <li><a href="#">Serie TV</a></li>
-            <li><a href="#">Recenti</a></li>
-        </ul>
-    </div>
+    <nav className={`navbar ${scrolling ? "scrolling" : ""}`}>
+      {/* Logo */}
+      <a href="/" className="logo">N</a>
+
+      {/* Links */}
+      <div className="nav-links">
+        <a href="#film">Film</a>
+        <a href="#serie-tv">Serie TV</a>
+        <a href="#recenti">Recenti</a>
+      </div>
+
+      {/* Buttons */}
+      <div className="nav-buttons">
+        <button><FaSearch size={20} /></button>
+        <button><FaThLarge size={20} /></button>
+        <button><FaUser Circle size={20} /></button>
+      </div>
+    </nav>
   );
-}
+};
 
 export default NavBar;
-
