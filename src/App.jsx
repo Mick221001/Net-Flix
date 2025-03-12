11da2bc7d6fb73212de99267f81cf012e98e4c73
Michele <michele5rosato@gmail.com>
2025-03-12 13:53:42 +0100

    NavBar fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,6 @@ function App() {
     <div>
       <NavBar />
       {/* Aggiungi un po' di contenuto per testare la navbar */}
-      <main style={{ padding: '20px' }}>
-        <h1>Benvenuto nella mia App!</h1>
-        <p>Scorri verso il basso per vedere l'effetto della navbar.</p>
-        <div style={{ height: '2000px' }}> {/* Aggiungi spazio per lo scroll */}
-          <p>Contenuto di esempio...</p>
-        </div>
-      </main>
     </div>
   );
 }
